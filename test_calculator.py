3cc95ad41fd7fa3a1804036e8e0b93e42b5c3c24
Basma Kamran <60095661@cna-qatar.edu.qa>
2023-03-14 18:51:46 +0300

    Add unit tests for calculator

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,5 @@
 import calculator
 
-
 class TestCalculator:
 
     def test_addition(self):
@@ -9,3 +8,6 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
+        
+    def test_multiplication(self):
+        assert 100 == calculator.multiply(10, 10)
